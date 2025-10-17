d0f0c376a3e25b47cb8d099322500e92b91a2f72
Sean Oijukye <s.oijukye@gmail.com>
2025-10-17 09:27:34 +0100

    Define the UI inside App

--- lib/main.dart
+++ lib/main.dart
@@ -2,84 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
   
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Sandwich Shop',
-//       theme: ThemeData(
-       
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       home: const MyHomePage(title: 'My Sandwich Shop'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-  
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-      
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return Scaffold(
-//       appBar: AppBar(
-        
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-        
-//         child: Column(
-          
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text('Welcome to my Sandwich shop!', style: TextStyle(fontWeight: FontWeight.bold)),
-//             const SizedBox(height: 8),
-//             Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), 
-//     );
-//   }
-}
-
+  }
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Sandwich Shop App',
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Sandwich Counter')),
+      body: const Center(
+        child: Text('Welcome to the Sandwich Shop!'),
+      ),
+    ),
+  );
 }
+  }
+
