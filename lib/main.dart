9de990cee4719d4efc85a32726693ab953afc1a0
Sean Oijukye <s.oijukye@gmail.com>
2025-10-17 09:31:30 +0100

    Use OrderItemDisplay in App

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ Widget build(BuildContext context) {
     home: Scaffold(
       appBar: AppBar(title: const Text('Sandwich Counter')),
       body: const Center(
-        child: Text('Welcome to the Sandwich Shop!'),
+        child: OrderItemDisplay(5, 'Footlong'),
       ),
     ),
   );
